e8a8292577ed8822e8533513090b7ac75569e9ee
Day Ahn <day@channel.io>
2025-03-24 15:43:11 +0900

    [Feat] caching reduced embedding vectors

--- script/run_clustering.py
+++ script/run_clustering.py
@@ -3,6 +3,9 @@ import dotenv
 import asyncio
 import logging
 import nest_asyncio
+import torch
+import numpy as np
+import os
 
 nest_asyncio.apply()
 dotenv.load_dotenv()
@@ -19,6 +22,7 @@ def main(
     start_date: str = None,
     end_date: str = None,
     subdirs: Optional[List[str]] = None,
+    cache_dir: Optional[str] = None,
 ):
     # Prepare dataset
     log_handler = LogHandler(logs_dir, subdirs, start_date, end_date)
@@ -30,19 +34,26 @@ def main(
         return
 
     # Embed dataset
-    loop = asyncio.get_event_loop()
-    try:
-        X = loop.run_until_complete(EmbeddingExtractor.async_get_embeddings(summaries))
-        X = reduce_dimensions(X, method="umap", n_components=64)
+    if os.path.exists(cache_dir):
+        X = torch.load(cache_dir, weights_only=False)
+        X = np.array(X)
+    else:
+        loop = asyncio.get_event_loop()
+        try:
+            X = loop.run_until_complete(EmbeddingExtractor.async_get_embeddings(summaries))
+            X = reduce_dimensions(X, method="umap", n_components=64)
+            torch.save(X, cache_dir)
+        finally:
+            loop.close()
 
-        # Cluster dataset
-        hybrid_cluster = HybridCluster(
-            X=X,
-            texts=summaries
-        )
-        hybrid_cluster.fit()
-    finally:
-        loop.close()
+    # Cluster dataset
+    hybrid_cluster = HybridCluster(
+        X=X,
+        texts=summaries
+    )
+    hybrid_cluster.fit()
+
+    import pdb; pdb.set_trace()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -50,6 +61,7 @@ if __name__ == "__main__":
     parser.add_argument("--subdirs", nargs="+", default=None)  # Remove type=list[str]
     parser.add_argument("--start_date", type=str, default=None)
     parser.add_argument("--end_date", type=str, default=None)
+    parser.add_argument("--cache_dir", type=str, default=None)
     args = parser.parse_args()
     
     logging.basicConfig(level=logging.INFO)
